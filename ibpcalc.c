8e6d06b6815b4f910bcc9efa453db19c4ca01379
Luiz Gustavo <luizgustavops@gmail.com>
2019-07-23 08:38:20 -0300

    Simplified the algorithm to store just the highest forward and backward peaks the the lowest forward and backward valleys. Added TRUE, FALSE, SUCCESS and FAIL definitions. Added the closest values calculation and the return of the numerics found.

--- ibpcalc.c
+++ ibpcalc.c
@@ -2,16 +2,34 @@
    The Ibp numerics calculation library implementation.
 */
 #include <stdio.h>
+#include <stdlib.h>
 #include <limits.h>
 #include "ibpcalc.h"
 
+// Local definitions
+#define TRUE 1
+#define FALSE 0
+
+#define SUCCESS 0
+#define FAIL -1
+
+// Because of the given IBP waveform characteristic
+// shape, we need to search only for 2 peaks and to
+// valleys forward and backward to find the nearest
+// systolic and diastolic pressure values.
 #define MAXSEARCH 2
+
+// Debug on/off
+#define print printf
+//#define print //
+
+// Local functions
 
 int isPeak(const char* buf, int size, int i)
 {
    if (i > 0 && i < size-1) {
       if (buf[i-1] < buf[i] && buf[i] > buf[i+1]) {
-         return 1;  
+         return TRUE;
       }
       // Forward search
       else if (buf[i-1] < buf[i] && buf[i] == buf[i+1]) {
@@ -21,7 +39,7 @@ int isPeak(const char* buf, int size, int i)
               j++;
 
           if (buf[j+1] < buf[j])
-              return 1;
+              return TRUE;
       }
       // Backward search
       else if (buf[i+1] < buf[i] && buf[i] == buf[i-1]) {
@@ -31,18 +49,18 @@ int isPeak(const char* buf, int size, int i)
               j--;
 
           if (buf[j-1] < buf[j])
-              return 1;
+              return TRUE;
       }
    }
 
-   return 0;
+   return FALSE;
 }
 
 int isValley(const char* buf, int size, int i)
 {
    if (i > 0 && i < size-1) {
       if (buf[i-1] > buf[i] && buf[i] < buf[i+1]) {
-         return 1;
+         return TRUE;
       }
       // Forward search
       else if (buf[i-1] > buf[i] && buf[i] == buf[i+1]) {
@@ -52,7 +70,7 @@ int isValley(const char* buf, int size, int i)
               j++;
 
           if (buf[j+1] > buf[j])
-              return 1;
+              return TRUE;
       }
       // Backward search
       else if (buf[i+1] > buf[i] && buf[i] == buf[i-1]) {
@@ -62,12 +80,12 @@ int isValley(const char* buf, int size, int i)
               j--;
 
           if (buf[j-1] > buf[j])
-              return 1;
+              return TRUE;
       }
 
    }
 
-   return 0;
+   return FALSE;
 }
 
 
@@ -80,81 +98,152 @@ int ibpCalc(const char* buf, int size, int index, struct IbpNumerics* const nume
       int index;
    };
 
-   struct Sample forwardMax[MAXSEARCH], forwardMin[MAXSEARCH],
-                 backwardMax[MAXSEARCH], backwardMin[MAXSEARCH];
-   int i = 0, iMax, iMin;
+   // Local variables to store the search result of the forward and backward peaks and valleys
+   struct Sample fwMax, fwMin, bwMax, bwMin;
 
-   printf("Size=%d, index=%d\n", size, index);
+   // Local variables to store the overall values of max and min found
+   const struct Sample* max = NULL;
+   const struct Sample* min = NULL;
+
+   // Local aux variables
+   int i = 0, maxSamples, iMax, iMin;
+
+   print("Size=%d, index=%d\n", size, index);
 
    // Initialize variables
-   for(i = 0; i < MAXSEARCH; i++) {
-      forwardMax[i].value = INT_MIN;
-      forwardMax[i].index = -1;
+   fwMax.value = INT_MIN;
+   fwMax.index = -1;
+   bwMax.value = INT_MIN;
+   bwMax.index = -1;
 
-      backwardMax[i].value = INT_MIN;
-      backwardMax[i].index = -1;
-
-      forwardMin[i].value = INT_MAX;
-      forwardMin[i].index = -1;
-
-      backwardMin[i].value = INT_MAX;
-      backwardMin[i].index = -1;      
-   }
+   fwMin.value = INT_MAX;
+   fwMin.index = -1;
+   bwMin.value = INT_MAX;
+   bwMin.index = -1;
 
    iMax = 0;
    iMin = 0;
-   // Search FORWARD for, at maximum, MAXSEARCH peaks or MAXSEARCH valleys
-   for(i = index; (i < size && (iMax < MAXSEARCH || iMin < MAXSEARCH)); i++) {
 
-      printf("FORWARD search index = %d, value = %d, iMax = %d, iMin = %d\n", i, buf[i], iMax, iMin);
+   // Limit the search to 6 seconds of waveform to find a heartbeat.
+   //
+   // A 10 bpm is the lowest heart rate this algorithm can work. This will limit
+   // the search despite of the size of the buffer informed to the algorithm,
+   // resulting in a cost of O(1).
+   // As we have 8ms between samples, the max samples to be searched will be
+   // 6000ms / 8ms = 750 samples, 375 samples for each side (forward and backward).
+   maxSamples = 375;
 
-      if (isPeak(buf, size, i)) {
-         forwardMax[iMax].value = buf[i];
-         forwardMax[iMax].index = i;
-         iMax++;
-         printf(" + Found Max\n");
+   // Search FORWARD for, at maximum, MAXSEARCH peaks or MAXSEARCH valleys.
+   for(i = index; (i < (index + maxSamples) && i < size && (iMax < MAXSEARCH && iMin < MAXSEARCH)); i++) {
+
+      print("FORWARD search index = %3d, value = %3d, iMax = %3d, iMin = %3d\n", i, buf[i], iMax, iMin);
+
+      if (isPeak(buf, index + maxSamples, i)) {
+
+         if (buf[i] > fwMax.value || (buf[i] == fwMax.value && abs(index - i) < abs(index - fwMax.index))) {
+             fwMax.value = buf[i];
+             fwMax.index = i;
+             print(" + Found Max [%d][%d]\n", buf[i], i);
+             iMax++;
+         }
+
       }
-      else if (isValley(buf, size, i)) {
-         forwardMin[iMin].value = buf[i];
-         forwardMin[iMin].index = i;
-         iMin++;
-         printf(" - Found Min\n");
+      else if (isValley(buf, index + maxSamples, i)) {
+
+         if (buf[i] < fwMin.value || (buf[i] == fwMin.value && abs(index - i) < abs(index - fwMin.index))) {
+            fwMin.value = buf[i];
+            fwMin.index = i;
+            print(" - Found Min [%d][%d]\n", buf[i], i);
+            iMin++;
+         }
+
       }
    }
 
-   printf("\n");
+   print("\n");
 
    iMax = 0;
    iMin = 0;
+
    // Search BACKWARD for, at maximum, MAXSEARCH peaks or MAXSEARCH valleys
-   for(i = index; (i > 0 && (iMax < MAXSEARCH || iMin < MAXSEARCH)); i--) {
+   for(i = index; (i > (index - maxSamples) && i >= 0 && (iMax < MAXSEARCH && iMin < MAXSEARCH)); i--) {
 
-      printf("BACKWARD search index = %d, value = %d, iMax = %d, iMin = %d\n", i, buf[i], iMax, iMin);
+      print("BACKWARD search index = %3d, value = %3d, iMax = %3d, iMin = %3d\n", i, buf[i], iMax, iMin);
 
-      if (isPeak(buf, size, i)) {
-         backwardMax[iMax].value = buf[i];
-         backwardMax[iMax].index = i;
-         iMax++;
-         printf(" + Found Max\n");
+      if (isPeak(buf, index + maxSamples, i)) {
+
+         // Check for peak with nearest index
+         if (buf[i] > bwMax.value || (buf[i] == bwMax.value && abs(index - i) < abs(index - bwMax.index))) {
+            bwMax.value = buf[i];
+            bwMax.index = i;
+            print(" + Found Max [%d][%d]\n", buf[i], i);
+            iMax++;
+         }
+
       }
-      else if (isValley(buf, size, i)) {
-         backwardMin[iMin].value = buf[i];
-         backwardMin[iMin].index = i;
-         iMin++;
-         printf(" - Found Min\n");
+      else if (isValley(buf, index + maxSamples, i)) {
+
+         // Check for valley with nearest index
+         if (buf[i] < bwMin.value || (buf[i] == bwMin.value && abs(index - i) < abs(index - bwMin.index))) {
+            bwMin.value = buf[i];
+            bwMin.index = i;
+            print(" - Found Min [%d][%d]\n", buf[i], i);
+            iMin++;
+         }
+
       }
    }
 
-   for(i = 0; i < MAXSEARCH; i++) {
-      printf("\nForwardMax = %d [%d], ForwardMin = %d [%d].\n", forwardMax[i].value, forwardMax[i].index,
-                                                                forwardMin[i].value, forwardMin[i].index);
+   // Debug
+   print("\nForwardMax = %d [%d], ForwardMin = %d [%d].\n", fwMax.value, fwMax.index,
+                                                            fwMin.value, fwMin.index);
+
+   print("\nBackwardMax = %d [%d], BackwardMin = %d [%d].\n", bwMax.value, bwMax.index,
+                                                              bwMin.value, bwMin.index);
+
+   // Calculate the closest peak
+   if (fwMax.index != -1 && bwMax.index != -1) {
+
+      if (abs(index - fwMax.index) < abs(index - bwMax.index))
+         max = &fwMax;
+      else
+         max = &bwMax;
+
    }
-   for(i = 0; i < MAXSEARCH; i++) {
-      printf("\nBackwardMax = %d [%d], BackwardMin = %d [%d].\n", backwardMax[i].value, backwardMax[i].index,
-                                                                  backwardMin[i].value, backwardMin[i].index);
+   else if (fwMax.index != -1)
+       max = &fwMax;
+   else if (bwMax.index != -1)
+       max = &bwMax;
+
+   // Calculate the closest valley
+   if (fwMin.index != -1 && bwMin.index != -1) {
+
+      if (abs(index - fwMin.index) < abs(index - bwMin.index))
+         min = &fwMin;
+      else
+         min = &bwMin;
+
+   }
+   else if (fwMin.index != -1)
+       min = &fwMin;
+   else if (bwMin.index != -1)
+       min = &bwMin;
+
+   // Check if valid values were found
+   if (max != NULL && min != NULL) {
+
+       print("\nMax = %d [%d], Min = %d [%d].\n", max->value, max->index, min->value, min->index);
+
+       numerics->sys      = max->value;
+       numerics->sysIndex = max->index;
+
+       numerics->dia      = min->value;
+       numerics->diaIndex = min->index;
+
+       return SUCCESS;
    }
    
-   return 0;
+   return FAIL;
 }
 
 
